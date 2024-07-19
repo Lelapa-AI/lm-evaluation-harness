6c806b3a9260308b19ddd2af6559feda1331d97b
Bonaventure F. P. Dossou <femipancrace.dossou@gmail.com>
2024-07-19 10:51:07 -0400

    Public Models

--- lm_eval/tasks/afrimmlu/fewshot.sh
+++ lm_eval/tasks/afrimmlu/fewshot.sh
@@ -1,8 +1,20 @@
-lm_eval --model hf \
-        --model_args pretrained=masakhane/African-ultrachat-alpaca  \
+models=(
+  dice-research/lola_v1
+  UBC-NLP/serengeti)
+
+for model in "${models[@]}"
+do
+  echo "Evaluating model: $model"
+  for fewshot in 0 2
+  do
+    export OUTPUT_DIR=results/$fewshot
+    mkdir -p "$OUTPUT_DIR"
+    lm_eval --model hf \
+        --model_args pretrained=${model},trust_remote_code=True,parallelize=True  \
         --tasks afrimmlu_direct_amh,afrimmlu_direct_eng,afrimmlu_direct_ewe,afrimmlu_direct_fra,afrimmlu_direct_hau,afrimmlu_direct_ibo,afrimmlu_direct_kin,afrimmlu_direct_lin,afrimmlu_direct_lug,afrimmlu_direct_orm,afrimmlu_direct_sna,afrimmlu_direct_sot,afrimmlu_direct_twi,afrimmlu_direct_wol,afrimmlu_direct_xho,afrimmlu_direct_yor,afrimmlu_direct_zul   \
-        --device cuda:0     \
+        --output_path "$OUTPUT_DIR" \
         --batch_size 1 \
-        --num_fewshot 0 \
-        --verbosity DEBUG \
-        --wandb_args project=afrimmlu
+        --num_fewshot $fewshot \
+        --verbosity DEBUG
+  done
+done
