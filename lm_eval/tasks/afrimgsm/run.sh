d581c9cf89eb90d6b0e397bf5811fde08ec6606b
Bonaventure F. P. Dossou <femipancrace.dossou@gmail.com>
2024-07-19 21:17:20 -0400

    Public Models

--- lm_eval/tasks/afrimgsm/run.sh
+++ lm_eval/tasks/afrimgsm/run.sh
@@ -12,7 +12,7 @@ do
     mkdir -p "$OUTPUT_DIR"
     lm_eval --model hf   \
         --model_args pretrained=${model},trust_remote_code=True,parallelize=True\
-        --tasks afrimgsm_en_cot_eng,mgsm_en_cot_en,afrimgsm_native_cot_eng,mgsm_native_cot_en,afrimgsm_direct_eng,mgsm_direct_en,afrimgsm_direct_native_eng  \
+        --tasks afrimgsm_en_cot_eng,mgsm_en_cot_en,mgsm_native_cot_en,afrimgsm_direct_eng,mgsm_direct_en  \
         --output_path "$OUTPUT_DIR" \
         --batch_size 1  \
         --num_fewshot $fewshot \
