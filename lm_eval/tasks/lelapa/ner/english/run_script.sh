a1626abcdfbdfc6b7e40b792ceb53c60ab73a0c1
jess <jessicaojo19@gmail.com>
2024-07-23 18:06:03 +0100

    improve ner and pos tasks

--- lm_eval/tasks/lelapa/ner/english/run_script.sh
+++ lm_eval/tasks/lelapa/ner/english/run_script.sh
@@ -30,23 +30,3 @@ do
             --log_samples
   done
 done
-
-
-lm_eval --model openai-chat-completions \
-        --model_args model=gpt-3.5-turbo \
-        --tasks ner_english_hausa,ner_english_isizulu,ner_english_swahili,ner_english_xhosa,ner_english_yoruba \
-        --output_path "./" \
-        --verbosity DEBUG \
-        --log_samples \
-        --limit 5
-
-
-lm_eval --model hf \
-        --model_args "pretrained=bigscience/mt0-small",trust_remote_code=True \
-        --tasks ner_english_hausa,ner_english_isizulu,ner_english_swahili,ner_english_xhosa,ner_english_yoruba \
-        --device cuda:0 \
-        --batch_size 16 \
-        --output_path "./" \
-        --verbosity DEBUG \
-        --log_samples \
-        --limit 5
